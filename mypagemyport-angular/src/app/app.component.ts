a8cb99d761dd3d7267d25e7db727f90544138a05
prathan.p <prathan.psong@gmail.com>
2025-03-06 11:01:18 +0700

    Add bootstrap theme in project

--- mypagemyport-angular/src/app/app.component.ts
+++ mypagemyport-angular/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from "./nav/nav.component";
+import { FooterComponent } from "./footer/footer.component";
+import { MainComponent } from "./main/main.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavComponent, FooterComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
