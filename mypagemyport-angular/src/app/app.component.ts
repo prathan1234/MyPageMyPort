e9962376475233d526dca8c74357eca4e5e04239
prathan.p <prathan.p@cubesofttech.com>
2025-03-10 13:17:38 +0700

    Add resume, project, contact page

--- mypagemyport-angular/src/app/app.component.ts
+++ mypagemyport-angular/src/app/app.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
 import { FooterComponent } from "./footer/footer.component";
-import { MainComponent } from "./main/main.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavComponent, FooterComponent, MainComponent],
+  imports: [RouterOutlet, NavComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- mypagemyport-angular/src/app/app.routes.ts
+++ mypagemyport-angular/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ProjectComponent } from './project/project.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'resume', component: ResumeComponent },
+  { path: 'projects', component: ProjectComponent },
+  { path: 'contact', component: ContactComponent },
+];
