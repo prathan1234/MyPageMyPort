0dcce95629e71ab122fb85ec3361f70318023583
prathan.p <prathan.p@cubesofttech.com>
2025-03-13 15:15:11 +0700

    Add Go to top button

--- mypagemyport-angular/src/app/app.component.ts
+++ mypagemyport-angular/src/app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
 import { FooterComponent } from "./footer/footer.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavComponent, FooterComponent],
+  imports: [RouterOutlet, NavComponent, FooterComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'mypagemyport';
+  showTopButton: boolean = false;
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    this.showTopButton = window.scrollY > 350;
+  }
+
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
