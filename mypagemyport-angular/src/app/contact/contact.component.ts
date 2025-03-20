681bd2e46218b0ff0de46bc1d2f5898e81bb617b
prathan.p <prathan.p@cubesofttech.com>
2025-03-20 17:28:19 +0700

    Update project

--- mypagemyport-angular/src/app/contact/contact.component.ts
+++ mypagemyport-angular/src/app/contact/contact.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  error: boolean = false;
+
+  submit() {
+    if (this.error) {
+      this.error = false;
+    } else {
+      this.error = true;
+    }
+  }
 
 }
